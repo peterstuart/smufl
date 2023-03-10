1759633e647f54b50bc8188a367dc8c6d9efb353
Peter Stuart <peter@peterstuart.org>
2023-03-10 17:16:33 -0500

    Add tests to GlyphData

--- smufl/src/glyph_data.rs
+++ smufl/src/glyph_data.rs
@@ -48,3 +48,83 @@ impl<T> GlyphData<T> {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rstest::*;
+
+    use super::*;
+
+    #[rstest]
+    #[case::empty(
+        [],
+        Glyph::NoteheadBlack,
+        None
+    )]
+    #[case::not_empty(
+        [(Glyph::NoteheadBlack, 1), (Glyph::NoteheadWhole, 2)],
+        Glyph::NoteheadBlack,
+        Some(1)
+    )]
+    fn try_get<const NUM: usize>(
+        #[case] values: [(Glyph, u64); NUM],
+        #[case] glyph: Glyph,
+        #[case] expected: Option<u64>,
+    ) {
+        let glyph_data: GlyphData<u64> = GlyphData {
+            data: values
+                .into_iter()
+                .map(|(glyph, value)| (GlyphOrUnknown::Glyph(glyph), value))
+                .collect(),
+        };
+
+        assert_eq!(glyph_data.try_get(glyph), expected);
+    }
+
+    #[rstest]
+    #[should_panic]
+    #[case::empty(
+        [],
+        Glyph::NoteheadBlack,
+        1 // not really expected
+    )]
+    #[case::not_empty(
+        [(Glyph::NoteheadBlack, 1), (Glyph::NoteheadWhole, 2)],
+        Glyph::NoteheadBlack,
+        1
+    )]
+    fn get<const NUM: usize>(
+        #[case] values: [(Glyph, u64); NUM],
+        #[case] glyph: Glyph,
+        #[case] expected: u64,
+    ) {
+        let glyph_data: GlyphData<u64> = GlyphData {
+            data: values
+                .into_iter()
+                .map(|(glyph, value)| (GlyphOrUnknown::Glyph(glyph), value))
+                .collect(),
+        };
+
+        assert_eq!(glyph_data.get(glyph), expected);
+    }
+
+    #[rstest]
+    #[case::empty([], [])]
+    #[case::not_empty(
+        [
+            (GlyphOrUnknown::Glyph(Glyph::NoteheadBlack), 1),
+            (GlyphOrUnknown::Unknown("Unknown".to_owned()), 2)
+        ],
+        ["Unknown"]
+    )]
+    fn unknown_glyphs<const NUM: usize, const EXPECTED_NUM: usize>(
+        #[case] values: [(GlyphOrUnknown, u64); NUM],
+        #[case] expected: [&str; EXPECTED_NUM],
+    ) {
+        let glyph_data: GlyphData<u64> = GlyphData {
+            data: values.into_iter().collect(),
+        };
+        let unknown_glyphs: Vec<_> = glyph_data.unknown_glyphs().collect();
+        assert_eq!(unknown_glyphs, expected);
+    }
+}
